f5f5f5ccc21fe4403524176d58743f66960a773f
Rajdeep Sharma <srajdeep2002@gmail.com>
2023-03-09 13:24:44 +0530

    event

--- src/pages/event/event.jsx
+++ src/pages/event/event.jsx
@@ -6,10 +6,228 @@ function Event() {
   return (
     <div class="EventSection">
 
-      <h1>Event</h1>
+    <h1 align='center'>Event</h1>
+    <div class="grid-container">
+    <div class="grid-item">
+    <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+    </div>
 
+
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+   </div>
+
+
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>  
+   </div>
+ 
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+   </div>
+
+
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+   </div>
+
+
+
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+   </div>
+
+
+
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+   </div>
+
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+   </div>
+
+
+
+   <div class="grid-item">
+   <div class="card-container">
+      <div className="card-btn">
+        <button>
+          <a href="#/" target="_blank">
+          <div class="image-container">
+              <img src="https://avatars.githubusercontent.com/u/71311777?v=4" alt="img"/>
+            </div>
+      <div className="card-date">
+        <p>Date of Event</p>
+      </div>
+      <div className="card-type">
+        <p>Mode of Event</p>
+      </div>
+      <div className="card-name">
+        <p>Event Name</p>
+      </div>
+          </a>
+        </button>
+      </div>
+      </div>
+   </div>
 
     </div>
+      
+    </div>
+
   )
 }
 
