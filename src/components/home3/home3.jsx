ebade0b5544da6f8bd8f6155fec837d975610d93
bLank-10 <uvulircuu@gmail.com@gmail.com>
2023-05-23 13:11:17 +0530

    update

--- src/components/home3/home3.jsx
+++ src/components/home3/home3.jsx
@@ -5,14 +5,13 @@ function Home3() {
   return (
   <div class="Styledhome3">
       <h2 class="headertekst">UPCOMING EVENTS</h2>
-      <div class="container">
-      <div class="image">
-        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="img" width={500} height={400}/>
+      <div class="wrapper">
+        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="" />
+        <div class="text-box">
+          <h2>EVENT NAME</h2>
+          <p>event description</p>
+        </div>
       </div>
-      <div class="text">
-        <p>This is our UPCOMING EVENTS</p>
-      </div>
-    </div>
   </div>
   )
 }
