7876742345ff4fce420f1a1f353ee8fccc4aff2d
Rajdeep Sharma <srajdeep2002@gmail.com>
2023-05-31 17:08:48 +0530

    improved

--- src/components/home2/home2.jsx
+++ src/components/home2/home2.jsx
@@ -7,12 +7,12 @@ function Home2() {
     <h2 class="headertekst">ABOUT US</h2>
 
     <p>
-    IEEE FCRIT stands for the Institute of Electrical and Electronics Engineers (IEEE) Fr. Conceicao Rodrigues Institute of Technology (FCRIT). It is an engineering college located in Mumbai, India, and is affiliated with the University of Mumbai. The IEEE FCRIT student branch is a chapter of IEEE, which is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity. The student branch is run by the students of the college, and it aims to provide a platform for students to enhance their technical skills and knowledge.
+    IEEE FCRIT stands for the Institute of Electrical and Electronics Engineers (IEEE) Fr. Conceicao Rodrigues Institute of Technology (FCRIT). It is an engineering college located in Navi Mumbai, India, and is affiliated with the University of Mumbai. The IEEE FCRIT student branch is a chapter of IEEE, which is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity. The student branch is run by the students of the college, and it aims to provide a platform for students to enhance their technical skills and knowledge.
     </p>
     
 
     <p>
-      The IEEE FCRIT student branch organizes various technical and non-technical events throughout the year, including workshops, seminars, webinars, and competitions. These events provide opportunities for students to learn new skills, interact with experts in their field, and showcase their talents.
+    IEEE FCRIT student branch is a chapter of IEEE, which is the world's largest technical professional organization dedicated to advancing technology for the benefit of humanity. The student branch is run by the students of the college, and it aims to provide a platform for students to enhance their technical skills and knowledge.The IEEE FCRIT student branch organizes various technical and non-technical events throughout the year, including workshops, seminars, webinars, and competitions. These events provide opportunities for students to learn new skills, interact with experts in their field, and showcase their talents.
     </p>
 
     <p>
--- src/components/home3/home3.jsx
+++ src/components/home3/home3.jsx
@@ -6,10 +6,15 @@ function Home3() {
   <div class="Styledhome3">
       <h2 class="headertekst">UPCOMING EVENTS</h2>
       <div class="wrapper">
-        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="" />
+        <div class="image">
+        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="" width="350"/>
+        </div>
         <div class="text-box">
-          <h2>EVENT NAME</h2>
-          <p>event description</p>
+          <h3>Event Name</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim neque a omnis veniam ducimus possimus cumque earum inventore incidunt fugit doloribus quibusdam delectus quos quasi temporibus culpa, et, ab consequuntur.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur maxime ratione atque facere iure recusandae veniam et, eveniet impedit ipsa cumque, esse in omnis expedita vitae tempore? Eveniet, mollitia doloremque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis dicta nam sed ea? Est doloremque aliquid necessitatibus? Consectetur odit unde harum odio! Fugit nam quam consectetur cupiditate, aliquam soluta!
+          </p>
         </div>
       </div>
   </div>
