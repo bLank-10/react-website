4ab921e305c9e5537bbfb72a723e28a7f8fd7b0f
bLank-10 <uvulircuu@gmail.com@gmail.com>
2023-06-14 21:03:42 +0530

    home3

--- src/components/home3/home3.jsx
+++ src/components/home3/home3.jsx
@@ -10,13 +10,28 @@ function Home3() {
         <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="" width="350"/>
         </div>
         <div class="text-box">
-          <h3>Event Name</h3>
+          <h3>Event 1 Name</h3>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim neque a omnis veniam ducimus possimus cumque earum inventore incidunt fugit doloribus quibusdam delectus quos quasi temporibus culpa, et, ab consequuntur.
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur maxime ratione atque facere iure recusandae veniam et, eveniet impedit ipsa cumque, esse in omnis expedita vitae tempore? Eveniet, mollitia doloremque.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis dicta nam sed ea? Est doloremque aliquid necessitatibus? Consectetur odit unde harum odio! Fugit nam quam consectetur cupiditate, aliquam soluta!
           </p>
         </div>
       </div>
+
+
+      <div class="wrapper">
+        <div class="image">
+        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="" width="350"/>
+        </div>
+        <div class="text-box">
+          <h3>Event 2 Name</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim neque a omnis veniam ducimus possimus cumque earum inventore incidunt fugit doloribus quibusdam delectus quos quasi temporibus culpa, et, ab consequuntur.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur maxime ratione atque facere iure recusandae veniam et, eveniet impedit ipsa cumque, esse in omnis expedita vitae tempore? Eveniet, mollitia doloremque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis dicta nam sed ea? Est doloremque aliquid necessitatibus? Consectetur odit unde harum odio! Fugit nam quam consectetur cupiditate, aliquam soluta!
+          </p>
+        </div>
+      </div>
+
   </div>
   )
 }
